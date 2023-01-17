14014cee5ca2ba5e43afabcc358a6ee3ada91031
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2023-01-17 22:05:24 +0100

    creating animated bursts

--- showcase/src/patterns/01.js
+++ showcase/src/patterns/01.js
@@ -26,6 +26,39 @@ const withClapAnimation = (WrappedComponent) => {
 				easing: mojs.easing.ease.out,
 			});
 
+			const triangleBurst = new mojs.Burst({
+				parent: '#clap',
+				radius: { 50: 95 },
+				count: 5,
+				angle: 30,
+				duration: timelineDuration,
+				children: {
+					shape: 'polygon',
+					radius: { 6: 0 },
+					stroke: 'rgba(211,54,0,0.5',
+					strokeWidth: 2,
+					angle: 210,
+					speed: 0.2,
+					delay: 30,
+					easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+				},
+			});
+
+			const circleBurst = new mojs.Burst({
+				parent: '#clap',
+				radius: { 50: 75 },
+				angle: 25,
+				duration: timelineDuration,
+				children: {
+					shape: 'circle',
+					fill: 'rgba(149,165,166,0.5)',
+					delay: 30,
+					speed: 0.2,
+					radius: { 3: 0 },
+					easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+				},
+			});
+
 			const countAnimation = new mojs.Html({
 				el: '#clapCount',
 				opacity: { 0: 1 },
@@ -48,6 +81,8 @@ const withClapAnimation = (WrappedComponent) => {
 				scaleButton,
 				countTotalAnimation,
 				countAnimation,
+				triangleBurst,
+				circleBurst,
 			]);
 			this.setState({ animateTimeline: newAnimationTimeline });
 		}
